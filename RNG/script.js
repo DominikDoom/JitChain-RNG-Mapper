8d79f1dceab32c21a600237d70a62fd27f906b09
DominikDoom <34448969+DominikDoom@users.noreply.github.com>
2018-12-20 15:20:27 +0100

    first push

---
+++ RNG/script.js
@@ -0,0 +1,39 @@
+// Generate Random Numbers
+var rndArray = Array.from({length: 10}, () => Math.floor(Math.random() * 51));
+console.log(rndArray);
+var sum = input.reduce((a, b) => a + b, 0);
+console.log(sum);
+var random = sum % 50;
+
+var ctx = document.getElementById("chart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: 'Verteilung',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
